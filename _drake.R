89b936b166681c3ae30975a4fc1a3ebe52f1985b
nguyenngocbinh <nguyenngocbinhneu@gmail.com>
2020-02-17 00:18:10 +0700

    hehe

--- _drake.R
+++ _drake.R
@@ -8,18 +8,26 @@ sourceDirectory("R")
 #lgr::get_logger("mlr3")$set_threshold("warn")
 
 # Create plans -----------------------------------------------------------------
-
 plan = bind_plans(clean_plan, task_plan, learner_plan, param_sets_plan,
-                  resampling_plan, measures_plan,  tuning_plan, train_plan
+                  resampling_plan, measures_plan,  tuning_plan, train_plan, predict_plan
 )
 
 # Set the config ---------------------------------------------------------------
 
-drake_config(plan, verbose = 2, lock_envir = FALSE,
-             # internal parallelization
-             prework = quote(future::plan(future.callr::callr, workers = 4)),
-             # logging
-             console_log_file = here::here("log/drake.log"))
+# drake_config(plan, verbose = 2, lock_envir = FALSE,
+#              # internal parallelization
+#              prework = quote(future::plan(future.callr::callr, workers = 4)),
+#              # logging
+#              console_log_file = here::here("log/drake.log"))
+
 
 make(plan)
 
+readd(rf_predict) %>%
+  as.data.table() %>%
+  select(id = row_id, label = prob.bad) %>%
+  mutate(id = id - 1) %>%
+  rio::export("results/rf_predict.csv")
+
+readd(graph)$plot(html = TRUE) %>% visNetwork::visInteraction()
+readd(glrn)$param_set %>% as.data.table()
--- _packages.R
+++ _packages.R
@@ -1,12 +1,12 @@
 # Initial
 options(repos = "http://cran.rstudio.org")
 have.packages <- installed.packages()
-cran.packages <- c("tidyverse", "inspectdf", "data.table", "mlr3verse", "here", "paradox" )
+cran.packages <- c("tidyverse", "inspectdf", "data.table", "mlr3verse", "here", "paradox", "drake" )
 to.install <- setdiff(cran.packages, have.packages[, 1])
 if (length(to.install) > 0) install.packages(to.install)
 
 #-----------------------------------------------------------------------------
 # require packages
-pkgs <- c("inspectdf", "readr", "dplyr", "tidyr", "magrittr", "data.table", "mlr3verse", "mlr3viz", "here", "R.utils")
+pkgs <- c("inspectdf", "readr", "dplyr", "tidyr", "magrittr", "data.table", "mlr3verse", "mlr3viz", "here", "R.utils", "drake")
 lapply(pkgs, function(pk) require(pk, character.only = TRUE))
 
