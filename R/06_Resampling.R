415a5a1f154400cc72d7b1132296201126698f39
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-03-19 11:16:43 +0700

    rename file

--- R/06_Resampling.R
+++ R/06_Resampling.R
@@ -1,13 +1,16 @@
+# guide
+# as.data.table(mlr_resamplings)
+
 resampling_plan = drake_plan(
   resampling_inner = target({
     rsmp_in = rsmp("cv")
-    rsmp_in$param_set$values = list(folds = 5)
+    rsmp_in$param_set$values = list(folds = 3)
     return(rsmp_in)
   }),
 
   resampling_outer = target({
     rsmp_out = rsmp("repeated_cv")
-    rsmp_out$param_set$values = list(folds = 2, repeats = 1)
+    rsmp_out$param_set$values = list(folds = 3, repeats = 1)
     return(rsmp_out)
   })
 )
