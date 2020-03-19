0fbb0f6836946060a831c801e9040959b49ed2f9
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-03-19 11:07:43 +0700

    update

--- _packages.R
+++ _packages.R
@@ -1,12 +1,11 @@
 # Initial
 options(repos = "http://cran.rstudio.org")
-have.packages <- installed.packages()
+have.packages <- installed.packages()[, 1]
 cran.packages <- c("tidyverse", "inspectdf", "data.table", "mlr3verse", "here", "paradox", "drake" )
-to.install <- setdiff(cran.packages, have.packages[, 1])
+to.install <- setdiff(cran.packages, have.packages)
 if (length(to.install) > 0) install.packages(to.install)
 
 #-----------------------------------------------------------------------------
 # require packages
 pkgs <- c("inspectdf", "readr", "dplyr", "tidyr", "magrittr", "data.table", "mlr3verse", "mlr3viz", "here", "R.utils", "drake")
 lapply(pkgs, function(pk) require(pk, character.only = TRUE))
-
