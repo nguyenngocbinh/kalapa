6ba2ce51310c24f352033b4b33deebea8695f61a
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-03-20 15:47:53 +0700

    export parameters and predict results at here

--- R/08_Train_predict.R
+++ R/08_Train_predict.R
@@ -8,9 +8,16 @@ train_plan = drake_plan(
 # Predict
 predict_plan = drake_plan(
   rf_predict = rf_train$predict(task_classif, row_ids = test_idx),
-  export = rf_predict %>%
+  #
+  export_result = rf_predict %>%
     as.data.table() %>%
     select(id = row_id, label = prob.bad) %>%
     mutate(id = id - 1) %>%
-    rio::export("results/xgboost_output.csv")
+    rio::export(paste0("results/result",stringr::str_replace_all(as.character(Sys.time()), ":", "-"), ".csv")),
+
+  # export results
+  export_parameter = rf_train$archive(unnest = "params") %>%
+    select(contains("classif")) %>%
+    rio::export(paste0("results/para", stringr::str_replace_all(as.character(Sys.time()), ":", "-"), ".xlsx"))
 )
+
