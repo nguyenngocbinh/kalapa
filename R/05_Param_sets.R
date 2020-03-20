7c74600a1432c7278aed0f474643e7a2a0a818df
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-03-20 15:46:39 +0700

    add more parameters

--- R/05_Param_sets.R
+++ R/05_Param_sets.R
@@ -5,13 +5,34 @@ param_sets_plan = drake_plan(
   ps = ParamSet$new(list(
     #ParamFct$new("classif.xgboost.booster", levels = c("gbtree", "gblinear", "dart")),
     ParamFct$new("classif.xgboost.booster", levels = "gblinear"),
-    ParamInt$new("classif.xgboost.scale_pos_weight", lower = 2, upper = 60),
-    ParamInt$new("classif.xgboost.max_depth", lower = 45, upper = 50),
-    ParamDbl$new("classif.xgboost.eta", lower = .01, upper = .1),
-    ParamInt$new("classif.xgboost.gamma", lower = 3, upper = 7)
+    ParamInt$new("classif.xgboost.scale_pos_weight", lower = 2, upper = 80),
+    ParamInt$new("classif.xgboost.max_depth", lower = 5, upper = 50),
+    ParamDbl$new("classif.xgboost.eta", lower = .01, upper = .7),
+    ParamInt$new("classif.xgboost.gamma", lower = 3, upper = 7),
+    ParamDbl$new("classif.xgboost.min_child_weight", lower = .01, upper = 20),
+    ParamInt$new("classif.xgboost.max_delta_step", lower = 1, upper = 10), # control imbalances
+    ParamDbl$new("classif.xgboost.subsample", lower = 0.5, upper = 0.8),
+    #ParamFct$new("classif.xgboost.feature_selector", levels = c("cyclic", "shuffle", "random", "greedy", "thrifty"))
+    ParamFct$new("classif.xgboost.feature_selector", levels = "thrifty"),
+    ParamInt$new("classif.xgboost.top_k", lower = 5, upper = 20)
+    # ParamInt$new("classif.xgboost.nrounds", lower = 100, upper = 500), # maximum number of iterations, or tree
 
   ))
 )
+
+# check results
+# readd(rf_train)$archive(unnest = "params")[, c("classif.xgboost.scale_pos_weight",
+#                                                "classif.xgboost.max_depth",
+#                                                "classif.xgboost.eta",
+#                                                "classif.xgboost.gamma",
+#                                                "classif.auc")] %>% arrange(-classif.auc)
+#
+#
+# readd(rf_train)$archive(unnest = "params")[, c("classif.xgboost.scale_pos_weight",
+#                                         "classif.xgboost.max_depth",
+#                                         "classif.xgboost.eta",
+#                                         "classif.xgboost.gamma",
+#                                         "classif.auc")] %>% cor()
 
 
 ## SET PARAMETER FOR RANGER
