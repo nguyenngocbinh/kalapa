dbd2a107fc454e0d13e375bd0f504f0f7feae318
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-03-23 15:30:31 +0700

    Update

--- _packages.R
+++ _packages.R
@@ -1,7 +1,8 @@
 # Initial
 options(repos = "http://cran.rstudio.org")
+# options(repos = "https://cloud.r-project.org")
 have.packages <- installed.packages()[, 1]
-cran.packages <- c("tidyverse", "inspectdf", "data.table", "mlr3verse", "here", "paradox", "drake", "magrittr", "R.utils" , "scorecard")
+cran.packages <- c("tidyverse", "inspectdf", "data.table", "mlr3verse", "here", "paradox", "drake" )
 to.install <- setdiff(cran.packages, have.packages)
 if (length(to.install) > 0) install.packages(to.install)
 
